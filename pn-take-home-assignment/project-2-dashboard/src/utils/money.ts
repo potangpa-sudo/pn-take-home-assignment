083437988d7b0721a3f59cadf7edc748292a906a
potangpa <paramacharoenphon@gmail.com>
2025-12-17 18:54:16 +0700

    chore: remove nested pn-take-home-assignment folder

--- pn-take-home-assignment/project-2-dashboard/src/utils/money.ts
+++
@@ -1,8 +0,0 @@
-export const formatCurrency = (amount: number): string => {
-    return new Intl.NumberFormat('th-TH', {
-        style: 'currency',
-        currency: 'THB',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(amount);
-};
